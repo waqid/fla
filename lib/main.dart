ff3e8d67126126f99451856dc5923f08b1ec9cfb
Nur Waqid Muhsinin <waqid.id@gmail.com>
2022-10-18 20:12:46 +0700

    The Fundamentals of Layout in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MaterialApp(
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue),
+      useMaterial3: true,
+    ),
     home: Scaffold(
       appBar: AppBar(
-        title: Text("Navbar!"),
+        title: Text("Wanna some tea?"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.cyan[300]),
+            child: Text("One"),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.green[300]),
+            child: Text("Two"),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.blue[300]),
+            child: Text("Three"),
+          ),
+        ],
       ),
     ),
   ));
